f2134c3c5fa4c5c4bcccbf6d685741ed2b5a3fa5
yash <iamyashsarvaiya@gmail.com>
2026-01-02 17:05:51 +0530

    [IMP] tailwind: snap and scroll demo

---
+++ app/usestate/snapdemo/page.tsx
@@ -0,0 +1,33 @@
+export default function StringPage(){
+    return (
+        <div className="h-screen overflow-y-auto snap-mandatory snap-y">
+            <div className="h-screen p-10 snap-start">
+                <div className="h-full border-2 border-green-400">
+
+                </div>
+            </div>
+            <div className="h-screen p-10 snap-start">
+                <div className="h-full border-2 border-green-400">
+
+                </div>
+            </div>
+            <div className="h-screen p-10 snap-start">
+                <div className="h-full border-2 border-green-400">
+
+                </div>
+            </div>
+            <div className="h-screen p-10 snap-start">
+                <div className="h-full border-2 border-green-400">
+
+                </div>
+            </div>
+            <div className="h-screen p-10 snap-start">
+                <div className="h-full border-2 border-green-400">
+
+                </div>
+            </div>
+
+        </div>
+        
+    )
+}
